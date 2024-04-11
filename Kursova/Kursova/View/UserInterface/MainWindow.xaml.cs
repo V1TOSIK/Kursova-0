5f7b92376868787e38f57d1612bf95bba3182529
V I T O S <vitalioostrovski@gmail.com>
2024-04-11 04:04:45 +0300

    Add project files.

---
+++ Kursova/Kursova/View/UserInterface/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Kursova
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ Kursova/Kursova/View/UserInterface/Pages/Authorization.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using Kursova.View.UserControls;
+
+namespace Kursova.View.UserInterface.Pages
+{
+  public partial class Authorization : Page
+  {
+    public Authorization()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Authorization_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+
+    private void Button_Registration_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+  }
+}
